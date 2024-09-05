48d6828ffed71d57e9f4732101ff27eae4001534
dyrex <Dimut280501@gmail.com>
2024-09-06 00:29:41 +0300

    Добавлены скрипты

--- src/main/resources/db/scripts/data/import_product_data.sql
+++ src/main/resources/db/scripts/data/import_product_data.sql
@@ -1,5 +1,5 @@
 insert into product
-VALUES (1,'Миссис пицца', 'Что-то там', 300,'PIZZA', '\test_image\1.jpg');
+VALUES (1,'Миссис пицца', 'Что-то там', 300,'PIZZA', 'test_image/1.jpg');
 
 insert into product
-VALUES (2, 'Пицца Астралис', 'Очередная пицца', 1000,'PIZZA', '\test_image\2.jpg');
+VALUES (2, 'Пицца Астралис', 'Очередная пицца', 1000,'PIZZA', 'test_image/2.jpg');
